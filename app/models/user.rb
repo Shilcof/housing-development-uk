bcef8e3b7e9587f684975875a50bdf3fd23b80db
Shilcof <robert.shilcof@gmail.com>
2020-12-08 16:17:50 +0000

    added custom validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,17 @@ class User < ActiveRecord::Base
     validates :last_name, presence: true
     validates :email, presence: true, uniqueness: true
 
-    validates :company, uniqueness: true, allow_blank: true
+    validates :company, presence: true, uniqueness: true, if: :developer?
+    validates :website, presence: true, uniqueness: true, if: :developer?
 
     validates_associated :developments
     has_secure_password
 
     has_many :developments, foreign_key: "developer_id"
+
+    def developer?
+        !!developer
+    end
 
     def full_name
         [first_name, last_name].join(" ")
