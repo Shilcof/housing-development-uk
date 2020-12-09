fa29bf43de19b9d7d5a9a4dc1a60bd72ce8994fd
Shilcof <robert.shilcof@gmail.com>
2020-12-09 15:05:59 +0000

    comment controller tidied

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,6 @@ class CommentsController < ApplicationController
     redirect_if_not_logged_in
   end
 
-  # GET: /comments
-  get "/comments" do
-    erb :"/comments/index.html"
-  end
-
-  # GET: /comments/new
-  get "/comments/new" do
-    erb :"/comments/new.html"
-  end
-
   post "/comments" do
     redirect_if_developer_or_no_route
     comment = current_user.comments.build(params[:comment])
@@ -21,21 +11,6 @@ class CommentsController < ApplicationController
       flash[:message] = comment.errors.full_messages.join(". ") + "."
     end
     redirect "#{params[:route]}"
-  end
-
-  # GET: /comments/5
-  get "/comments/:id" do
-    erb :"/comments/show.html"
-  end
-
-  # GET: /comments/5/edit
-  get "/comments/:id/edit" do
-    erb :"/comments/edit.html"
-  end
-
-  # PATCH: /comments/5
-  patch "/comments/:id" do
-    redirect "/comments/:id"
   end
 
   delete "/comments/:id" do
@@ -48,18 +23,6 @@ class CommentsController < ApplicationController
   end
 
   private
-  def redirect_if_not_developer
-    if !current_user.developer
-      redirect "/"
-    end
-  end
-
-  def redirect_if_wrong_developer(developer)
-    if current_user != developer
-      redirect "/"
-    end
-  end
-
   def redirect_if_developer_or_no_route
     if !params[:route] || current_user.developer
       redirect "/"
